81a4825cb4bd3130f1ecd353d61bded6176c2d08
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-10-14 16:43:31 +0900

    MainScreen UI 업데이트

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -9,15 +9,44 @@ class MainScreen extends StatelessWidget {
         title: Text('Main Screen'),
       ),
       body: Center(
-        child: RaisedButton(
-          color: Colors.red,
-          child: Text('This is Main Screen'),
-          onPressed: () {
-            // Navigator.push(context, MaterialPageRoute(builder: (context) {
-            //   return Screen2();
-            // }));
-            print('main screen button pressed');
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 12,
+              child: Center(
+                child: Text(
+                  "This is Main Page",
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: RaisedButton(
+                color: Colors.purple,
+                child: Text('영상 유언 남기기'),
+                onPressed: () {
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  //   return Screen2();
+                  // }));
+                  print('main screen button pressed');
+                },
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: RaisedButton(
+                color: Colors.blue,
+                child: Text('저장된 유언'),
+                onPressed: () {
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  //   return Screen2();
+                  // }));
+                  print('main screen button pressed');
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
